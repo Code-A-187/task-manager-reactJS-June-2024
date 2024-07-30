bf32ba00b494b8b72df097b8d63f9fbb4fdf3754
Anton Simeonov <aplamenov@gmail.com>
2024-07-30 16:03:33 +0300

    configure react and tailwind dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
-  
+
   return (
-    <>
-      <h1>Starting again!</h1>
-      <button>Start</button>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Task Manager System with React
+    </h1>
+  
   )
 }
 
