773e9bbd84e44401f1145b6be217cb43c8c75bea
Anton Simeonov <aplamenov@gmail.com>
2024-08-01 15:11:58 +0300

    tasks requests from api

---
+++ client/src/api/tasks-api.js
@@ -0,0 +1,40 @@
+import request from './requester';
+
+const BASE_URL ='http://localhost:3030/data/tasks';
+
+export const getAll = async () => {
+    try {
+        const result = await request.get(BASE_URL);
+        const tasks = Object.values(result);
+        return tasks;
+    } catch (err) {
+        console.error('Failed to fetch tasks:', err);
+        throw new Error('Unable to fetch tasks');
+    }
+};
+
+export const getOne = async (taskId) => {
+    try {
+        return await request.get(`${BASE_URL}/${taskId}`);
+    } catch (err) {
+        console.error(`Failed to fetch task with ID ${taskId}:`, err);
+        throw new Error(`Unable to fetch task with ID ${taskId}`);
+    }
+};
+
+export const create = async (taskData) => {
+    try {
+        return await request.post(BASE_URL, taskData);
+    } catch (err) {
+        console.error('Failed to create task:', err);
+        throw new Error('Unable to create task');
+    }
+};
+
+const tasksAPI = {
+    getAll,
+    getOne,
+    create
+};
+
+export default tasksAPI;
--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import Task from "../task/Task";
 
 export default function Home() {
+  const [tasks, tasksState] = useState();
+
   
   return (
     <div className="bg-gray-50 px-4 py-12 font-[sans-serif]">
     <div className="max-w-6xl mx-auto">
       <div className="grid lg:grid-cols-3 md:grid-cols-2 max-md:max-w-lg mx-auto gap-8">
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
+          <Task />
       </div>
     </div>
   </div>
