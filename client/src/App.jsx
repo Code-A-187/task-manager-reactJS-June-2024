363edeaca319688ec215cc56b58a6e0f759cf9f9
Anton Simeonov <aplamenov@gmail.com>
2024-08-02 14:18:02 +0300

    create task route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,24 @@
-import { Route, Routes } from "react-router-dom"
-import Header from "./Header"
+import { Route, Router, Routes } from "react-router-dom"
 import Home from "./components/home/Home"
-import SideBar from "../src/components/side-bar/SideBar"
 import UserLogin from "../src/components/user-login/UserLogin"
 import UserRegister from "../src/components//user-register/UserRegister"
+import Layout from "./Layout"
+import CreateTaskModal from "./components/create-task/CreateTaskModal"
 
 function App() {
 
   return (
-    <>
-      <Header />
 
-      <Routes>        
-        <Route path="/"element={ <Home /> } />
-        <Route path="/login"element={ <UserLogin /> } />
-        <Route path="/register"element={ <UserRegister /> } />
-        <Route path="/sidebar"element={ <SideBar /> } />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/regiter" element={<UserRegister />} />
+          <Route path="/tasks/crete" element={<CreateTaskModal />} />
+        </Route>
       </Routes>
 
-    </>
+  );
+};
   
-  )
-}
-
 export default App
