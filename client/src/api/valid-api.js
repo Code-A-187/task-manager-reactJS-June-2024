4d390121fd48c0c468ff7487ebb1361ec65a8f46
Anton Simeonov <aplamenov@gmail.com>
2024-08-14 17:42:50 +0300

    validation of task form

--- client/src/api/valid-api.js
+++ client/src/api/valid-api.js
@@ -18,32 +18,52 @@ export function validateLogin(values) {
     return errors;
 }
 
-
-export function validateRegister (values) {
-    
+export function validateRegister(values) {
     const errors = {};
 
-    // Full Name validation
-    if (!values.fullName) {
+    if(!values.fullName) {
         errors.fullName = 'Full Name is required';
     } else if (values.fullName.length < 3) {
         errors.fullName = 'Full Name must be at least 3 characters long';
     }
 
-    // Email validation
-    if (!values.email) {
+    if (!values.email){
         errors.email = 'Email is required';
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-        errors.email = 'Email address is invalid';
+    } else if (!/\S+@\S+\+/.test(values.email)) {
+        errors.email = 'Email address is invalid'
     }
 
-    // Password validation
     if (!values.password) {
         errors.password = 'Password is required';
-    } else if (values.password.length < 6) {
-        errors.password = 'Password must be at least 6 characters long';
+    }   else if (values.password.length < 6) {
+        errors.passsword = 'Password must be at least 6 characters long'
     }
 
     return errors;
 };
+
+
+export function validateTaskForm(values) {
+    const errors = {};
+
+    // Validate Title
+    if (!values.title) {
+        errors.title = 'Title is required';
+    } else if (values.title.length < 3) {
+        errors.title = 'Title must be at least 3 characters long';
+    } else if (values.title.length > 50) {
+        errors.title = 'Title cannot exceed 20 characters';
+    }
+
+    // Validate Description
+    if (!values.description) {
+        errors.description = 'Description is required';
+    } else if (values.description.length < 10) {
+        errors.description = 'Description must be at least 10 characters long';
+    } else if (values.description.length > 200) {
+        errors.description = 'Description cannot exceed 100 characters';
+    }
+
+    return errors;
+}
 
